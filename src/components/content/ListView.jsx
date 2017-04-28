bca5f712de2fa880f0e323dc8bf5fca2c8f34fe8
Sandeep Reddy <msande1996@gmail.com>
2017-04-28 16:38:26 +0530

    Add black box;Mobile friendly

--- src/components/content/ListView.jsx
+++ src/components/content/ListView.jsx
@@ -5,20 +5,20 @@ class ListView extends Component {
 	constructor(props) {
 		super(props);
 		this.populateViews = this.populateViews.bind(this);
-		this.fontsAvail = ["Dancing Script","Indie Flower","Kaushan Script","Lobster","Pacifico"];
+		this.fontsAvail = ["Dancing Script", "Indie Flower", "Kaushan Script", "Lobster", "Pacifico"];
 	}
 
-	cleanQuote(quote){
+	cleanQuote(quote) {
 		return quote
-			.replace("<p>","")
-			.replace("</p>","")
-			.replace(/&#8217;/g,"'");
+			.replace("<p>", "")
+			.replace("</p>", "")
+			.replace(/&#8217;/g, "'");
 	}
 
 	populateViews() {
 		return this.props.quotes.map((quote, index) => {
 			let divStyle = {
-				fontFamily : this.fontsAvail[Math.floor(Math.random()*5)]
+				fontFamily: this.fontsAvail[Math.floor(Math.random() * 5)]
 			};
 			return (
 				<div className="item" key={quote.ID}>
